aa00ee0239b0485bbcc21d68b03529108bceade1
Emmanuel Chauvel <emmanuel.ch20@gmail.com>
2020-09-30 11:51:13 +0200

    Creation of structure

--- src/api/Capteur.java
+++ src/api/Capteur.java
@@ -2,9 +2,27 @@ package api;
 
 public interface Capteur {
 
+    /**
+     *
+     * @param o
+     */
     public void attach(Observer o);
+
+    /**
+     *
+     * @param o
+     */
     public void detach(Observer o);
+
+    /**
+     *
+     * @return
+     */
     public int getValue();
+
+    /**
+     * Increment the counter
+     */
     public void tick();
 
 }
--- src/implementation/CapteurImpl.java
+++ src/implementation/CapteurImpl.java
@@ -1,6 +1,26 @@
 package implementation;
 
 import api.Capteur;
+import api.Observer;
 
 public class CapteurImpl implements Capteur {
+    @Override
+    public void attach(Observer o) {
+
+    }
+
+    @Override
+    public void detach(Observer o) {
+
+    }
+
+    @Override
+    public int getValue() {
+        return 0;
+    }
+
+    @Override
+    public void tick() {
+
+    }
 }
