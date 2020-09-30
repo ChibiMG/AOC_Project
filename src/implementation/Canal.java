d192bf984f3f9373f416af7291aeccb4ee212c71
MaudG <maudg35@outlook.fr>
2020-09-30 11:45:36 +0200

    Implement methods in Canal.java

--- src/implementation/Canal.java
+++ src/implementation/Canal.java
@@ -1,7 +1,27 @@
 package implementation;
 
 import api.Capteur;
+import api.Observer;
 import api.ObserverdeCapteur;
 
 public class Canal implements Capteur, ObserverdeCapteur {
+    @Override
+    public void attach(Observer o) {
+        
+    }
+
+    @Override
+    public void detach(Observer o) {
+
+    }
+
+    @Override
+    public int getValue() {
+        return 0;
+    }
+
+    @Override
+    public void tick() {
+
+    }
 }
--- src/implementation/Update.java
+++ src/implementation/Update.java
@@ -1,6 +1,4 @@
 package implementation;
 
-import api.Callable;
-
 public class Update {
 }
