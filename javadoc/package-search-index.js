d46a65011f56190df31d0e5463c5f6018f3847ec
Emmanuel Chauvel <emmanuel.ch20@gmail.com>
2021-01-24 21:09:33 +0100

    Regenerate jar and javadoc

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"api"},{"l":"implementation"},{"l":"test"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"api"},{"l":"implementation"},{"l":"main"},{"l":"test"}]
